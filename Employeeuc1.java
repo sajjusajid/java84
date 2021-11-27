cda8b8b1dbde20457ae8b6f51703b8ff3b730dc6
sajjusajid <mdsajid599@gamil.com>
2021-11-27 14:58:22 +0530

    adding file Employeeuc1

---
+++ Employeeuc1.java
@@ -0,0 +1,12 @@
+public class Employeeuc1 {
+	public static void main (String[] args){
+	//constants
+	int IS_FULL_TIME = 1;
+	//computation
+	 double empCheck = Math.floor(Math.random() *10) % 2;
+	if (empCheck ==IS_FULL_TIME)
+	System.out.println("Emp is present");
+	else
+	System.out.println("emp is absent");
+   }
+}
